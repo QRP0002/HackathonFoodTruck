41661b78b0a7d7b2a6d8f155426f43c5da2844cf
Gregor <gmbordelon@msn.com>
2015-02-21 15:50:24 -0600

    update

--- app/src/main/java/Manager.java
+++ app/src/main/java/Manager.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Created by gmbordelon on 2/21/2015.
@@ -7,6 +8,7 @@ public class Manager {
 
     ArrayList<Customer> customerArray = new ArrayList<>();
     ArrayList<Truck> truckArray = new ArrayList<>();
+    Scanner userInput = new Scanner(System.in);
 
 
     public void createCustomer(String name, ArrayList recOrder, double location)
@@ -15,16 +17,34 @@ public class Manager {
         customerArray.add(tempCustomer);
     }
 
-    public void createTruck(String name, String phoneNum, double Xlocation, double Ylocation, String userName, String userPassword)
+    public void createTruck()
     {
+        String name, phoneNum, userName, userPassword;
+        double Xlocation, Ylocation;
+        System.out.println("Enter the truck's name: ");
+        name = userInput.next();
+        System.out.println("Enter the truck's phone number: ");
+        phoneNum = userInput.next();
+        System.out.println("Enter the truck's Xlocation: ");
+        Xlocation = userInput.nextDouble();
+        System.out.println("Enter the truck's Ylocation: ");
+        Ylocation = userInput.nextDouble();
+        System.out.println("Enter the trucks login username: ");
+        userName = userInput.next();
+        System.out.println("Enter the truck's login password: ");
+        userPassword = userInput.next();
+
+
         Truck tempTruck = new Truck(name, phoneNum, Xlocation, Ylocation, userName, userPassword);
         truckArray.add(tempTruck);
     }
 
+    /*
     public void createFood(String name, double salePrice, int quantity, int amtPerCase)
     {
         Food tempFood = new Food(name, salePrice, quantity, amtPerCase);
     }
+    */
 
 
 
